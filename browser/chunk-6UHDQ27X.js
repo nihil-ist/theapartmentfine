d8fe1a706ce19c4529e6022f5c28dc45c87c87e1
Diego Zamora Delgadillo <al261855@edu.uaa.mx>
2024-06-26 20:48:01 -0600

    Auto-generated commit

---
+++ browser/chunk-6UHDQ27X.js
@@ -0,0 +1,1 @@
+import"./chunk-JWXQZIVW.js";import"./chunk-YT5AF7SN.js";import"./chunk-SIAVTO45.js";
---
+++ browser/chunk-OR63XTXS.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-UNA7QTYE.js";import"./chunk-SIAVTO45.js";export default a();
